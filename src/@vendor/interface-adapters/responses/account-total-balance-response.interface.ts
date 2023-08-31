d8e0c8ad87084baa7c0d3e49a405db5f7e6ffcf9
Franck DAKIA <dakiafranck@gmail.com>
2023-08-31 01:47:04 +0000

    Formatting

--- src/@vendor/interface-adapters/responses/account-total-balance-response.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface IAccountTotalBalanceResponse {
-  clientId: string;
-  balance: number;
-}
--- src/@vendor/utils/number.utility.ts
+++ src/@vendor/utils/number.utility.ts
@@ -2,46 +2,10 @@ import { BigNumber } from 'bignumber.js';
 import { customAlphabet } from 'nanoid';
 
 /**
- * Calculate mobile money operator fee
- */
-export const MMOperatorFee = (amount: number, operatorRate: number, stampFee: number): number => {
-  return new BigNumber(amount).multipliedBy(operatorRate).dividedBy(100).plus(stampFee).decimalPlaces(0).toNumber();
-};
-
-/**
  * Calculate mobile money cashin fee
  */
-export const getAmountFee = (amount: number, cardLoadRate: number): number => {
-  return new BigNumber(amount).multipliedBy(cardLoadRate).dividedBy(100).decimalPlaces(0).toNumber();
-};
-
-/**
- * Calculate mobile money billing fee
- */
-export const MMBillingFee = (amount: number, billingRate: number): number => {
-  return new BigNumber(amount).multipliedBy(billingRate).dividedBy(100).decimalPlaces(0).toNumber();
-};
-
-/**
- * Transform to number
- * @param value
- */
-export const transformToNumber = (value: number): number => {
-  return new BigNumber(value, 10).decimalPlaces(0).toNumber();
-};
-
-/**
- * Make minus operation
- */
-export const makeMinus = (value: number, valueToRemove: number): number => {
-  return new BigNumber(value).minus(valueToRemove).toNumber();
-};
-
-/**
- * Make addition
- */
-export const makeAddition = (value: number, valueToRemove: number): number => {
-  return new BigNumber(value).plus(valueToRemove).toNumber();
+export const getAmountFee = (amount: number, rate: number): number => {
+  return new BigNumber(amount).multipliedBy(rate).dividedBy(100).decimalPlaces(0).toNumber();
 };
 
 /**
